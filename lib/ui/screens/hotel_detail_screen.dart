c5366a386a58b278d6297a64eeecb5ce87d597aa
yumayuma708 <yumaskywalker708@gmail.com>
2024-03-19 12:09:13 +0900

    Google Mapsの統合とホテル詳細画面のUI改善

--- lib/ui/screens/hotel_detail_screen.dart
+++ lib/ui/screens/hotel_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:aitrip/services/hotel_model_service.dart.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HotelDetail extends StatelessWidget {
   final Hotel hotel;
@@ -7,9 +8,83 @@ class HotelDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final String googleMapsUrl =
+        "https://www.google.com/maps/@${hotel.latitude},${hotel.longitude},14z";
+
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: (MediaQuery.of(context).size.height -
+                        kToolbarHeight -
+                        MediaQuery.of(context).padding.top) /
+                    5,
+                child: Image.network(
+                  hotel.hotelImageUrl,
+                  fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress == null) return child;
+                    return const Center(child: CircularProgressIndicator());
+                  },
+                  errorBuilder: (context, error, stackTrace) {
+                    return const Center(child: Icon(Icons.error));
+                  },
+                ),
+              ),
+              SizedBox(
+                height: (MediaQuery.of(context).size.height -
+                        kToolbarHeight -
+                        MediaQuery.of(context).padding.top) /
+                    5,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          Text(
+                            hotel.hotelName,
+                            style: const TextStyle(fontSize: 24),
+                          ),
+                          const SizedBox(height: 16),
+                          Row(
+                            children: [
+                              const Icon(Icons.favorite_rounded),
+                              Text(
+                                '${hotel.reviewAverage.toStringAsFixed(1)} / 5',
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: WebViewWidget(
+                        controller: WebViewController(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Text(
+                '2人、3月10日〜3月12日、2泊',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
