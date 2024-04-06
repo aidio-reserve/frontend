248453e05e1aca12bd0925de0a37dc8bac03cdc5
yumayuma708 <yumaskywalker708@gmail.com>
2024-04-06 20:50:07 +0900

    refactor: chatRowのファイルを分ける

--- lib/ui/screens/chat_screen.dart
+++ lib/ui/screens/chat_screen.dart
@@ -3,6 +3,7 @@ import 'package:aitrip/data/repositories/chat_repository.dart';
 import 'package:aitrip/models/messages.dart';
 import 'package:aitrip/providers/thread_id_provider.dart';
 import 'package:aitrip/services/hotel_service.dart';
+import 'package:aitrip/ui/components/chat_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_chat_bubble/chat_bubble.dart';
@@ -129,64 +130,5 @@ class ChatScreen extends ConsumerWidget {
             )
           ],
         )));
-  }
-
-  Widget userRow(BuildContext context, String message) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Flexible(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 38.0),
-            child: ChatBubble(
-              clipper: ChatBubbleClipper8(type: BubbleType.sendBubble),
-              alignment: Alignment.topRight,
-              margin: const EdgeInsets.only(top: 20),
-              backGroundColor: Theme.of(context).colorScheme.primaryContainer,
-              child: Text(
-                message,
-                style: TextStyle(
-                    color: Theme.of(context).colorScheme.onPrimaryContainer),
-                softWrap: true,
-                overflow: TextOverflow.clip,
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(width: 8),
-        const Icon(
-          Icons.face_rounded,
-          size: 30,
-        ),
-      ],
-    );
-  }
-
-  Widget serverRow(BuildContext context, String message) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Icon(
-          Icons.support_agent_rounded,
-          size: 30,
-        ),
-        const SizedBox(width: 8),
-        Flexible(
-          child: Padding(
-            padding: const EdgeInsets.only(right: 38.0),
-            child: ChatBubble(
-              clipper: ChatBubbleClipper8(type: BubbleType.receiverBubble),
-              margin: const EdgeInsets.only(top: 20),
-              backGroundColor: Theme.of(context).colorScheme.tertiaryContainer,
-              child: Text(
-                message,
-                style: TextStyle(
-                    color: Theme.of(context).colorScheme.onTertiaryContainer),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
   }
 }
