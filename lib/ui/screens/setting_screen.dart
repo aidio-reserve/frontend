dca2ed45338bb65cf0c6de7e5022ec843568879d
yumayuma708 <yumaskywalker708@gmail.com>
2024-05-06 20:39:27 +0900

    色を変更

--- lib/ui/screens/setting_screen.dart
+++ lib/ui/screens/setting_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:aitrip/providers/theme_provider.dart';
 import 'package:settings_ui/settings_ui.dart';
 
 class SettingScreen extends ConsumerWidget {
@@ -9,13 +8,15 @@ class SettingScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final themeNotifier = ref.watch(themeProvider);
-
     return Scaffold(
         appBar: AppBar(
           title: const Text('設定'),
         ),
         body: SettingsList(
+          lightTheme: const SettingsThemeData(
+            settingsListBackground: Color(0xFFF2F2F7),
+            settingsSectionBackground: Colors.white,
+          ),
           sections: [
             SettingsSection(
               tiles: [
@@ -30,7 +31,7 @@ class SettingScreen extends ConsumerWidget {
                 SettingsTile.navigation(
                   leading: Icon(
                     Icons.support_agent_rounded,
-                    color: Theme.of(context).colorScheme.secondary,
+                    color: Theme.of(context).colorScheme.primary,
                   ),
                   title: const Text('AIの名前の変更'),
                   onPressed: (context) {},
@@ -38,47 +39,47 @@ class SettingScreen extends ConsumerWidget {
                 SettingsTile.navigation(
                   leading: Icon(
                     Icons.monitor_rounded,
-                    color: Theme.of(context).colorScheme.tertiary,
+                    color: Theme.of(context).colorScheme.primary,
                   ),
                   title: const Text('画面・音声'),
                   onPressed: (context) {},
                 ),
                 SettingsTile.navigation(
-                  leading: const Icon(
+                  leading: Icon(
                     Icons.attach_money_rounded,
-                    color: Colors.black,
+                    color: Theme.of(context).colorScheme.primary,
                   ),
                   title: const Text('決済手段'),
                   onPressed: (context) {},
                 ),
                 SettingsTile.navigation(
-                  leading: const Icon(
+                  leading: Icon(
                     Icons.help_rounded,
-                    color: Colors.black,
+                    color: Theme.of(context).colorScheme.primary,
                   ),
                   title: const Text('ヘルプ'),
                   onPressed: (context) {},
                 ),
                 SettingsTile.navigation(
-                  leading: const Icon(
+                  leading: Icon(
                     Icons.description_rounded,
-                    color: Colors.black,
+                    color: Theme.of(context).colorScheme.primary,
                   ),
                   title: const Text('このアプリについて'),
                   onPressed: (context) {},
                 ),
                 SettingsTile.navigation(
-                  leading: const Icon(
+                  leading: Icon(
                     Icons.gavel_rounded,
-                    color: Colors.black,
+                    color: Theme.of(context).colorScheme.primary,
                   ),
                   title: const Text('利用規約'),
                   onPressed: (context) {},
                 ),
                 SettingsTile.navigation(
-                  leading: const Icon(
+                  leading: Icon(
                     Icons.privacy_tip_rounded,
-                    color: Colors.black,
+                    color: Theme.of(context).colorScheme.primary,
                   ),
                   title: const Text('プライバシーポリシー'),
                   onPressed: (context) {},
@@ -86,13 +87,6 @@ class SettingScreen extends ConsumerWidget {
               ],
             ),
           ],
-        )
-        // body: Center(
-        //   child: ElevatedButton(
-        //     onPressed: () => themeNotifier.toggleTheme(),
-        //     child: const Text('テーマを切り替える'),
-        //   ),
-        // ),
-        );
+        ));
   }
 }
