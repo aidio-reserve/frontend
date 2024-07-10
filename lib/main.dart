4dded39b99dbc09fcc3f0fb7fe4d2ab7c488e9ea
yumayuma708 <yumaskywalker708@gmail.com>
2024-07-11 00:32:28 +0900

    feat: Update dependencies for cloud_firestore and firebase_core

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:aitrip/data/firebase/firestore.dart';
 import 'package:aitrip/data/repositories/start_repository.dart';
 import 'package:aitrip/providers/thread_id_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'trip_app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'data/firebase/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
+  // final firestoreService = FirestoreService();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     ProviderScope(
