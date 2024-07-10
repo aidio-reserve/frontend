c83622946da2659afe952237cce76ab782c92310
yumayuma708 <yumaskywalker708@gmail.com>
2024-07-10 18:17:37 +0900

    hotel_optionを受け取る部分を実装

--- lib/models/messages.dart
+++ lib/models/messages.dart
@@ -4,9 +4,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class Message {
   final String text;
   final bool isSender;
+  final Map<String, dynamic> hotelOption;
   final int displayHotel;
   Message(
-      {required this.text, required this.isSender, required this.displayHotel});
+      {required this.text,
+      required this.isSender,
+      required this.hotelOption,
+      required this.displayHotel});
 }
 
 class MessageListNotifier extends StateNotifier<List<Message>> {
@@ -17,15 +21,21 @@ class MessageListNotifier extends StateNotifier<List<Message>> {
               text:
                   " こんにちは！どのような旅行をご希望ですか？\n \n  例. \t 北海道で海鮮丼を食べたい！ \n  例. \t 森林浴がしたい",
               isSender: false,
+              hotelOption: <String, dynamic>{},
               displayHotel: 0)
         ]);
 
-  void addMessage(String text, bool isSender, int displayHotel) {
+  void addMessage(String text, bool isSender, Map<String, dynamic> hotelOption,
+      int displayHotel) {
     state = [
       ...state,
-      Message(text: text, isSender: isSender, displayHotel: displayHotel)
+      Message(
+          text: text,
+          isSender: isSender,
+          hotelOption: hotelOption,
+          displayHotel: displayHotel)
     ];
     debugPrint(
-        'Updated Message List:【 ${state.map((message) => 'Text: ${message.text}, IsSender: ${message.isSender}, DisplayHotel: ${message.displayHotel}').toList()} 】');
+        'Updated Message List:【 ${state.map((message) => 'text: ${message.text}, isSender: ${message.isSender}, hotelOption: ${message.hotelOption}, displayHotel: ${message.displayHotel}').toList()} 】');
   }
 }
