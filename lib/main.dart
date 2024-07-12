43d159a2c508f0f8a5c07c505b685da6ff978082
yumayuma708 <yumaskywalker708@gmail.com>
2024-07-12 23:48:46 +0900

    リファクタリング

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:aitrip/data/repositories/start_repository.dart';
 import 'package:aitrip/providers/thread_id_provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'trip_app.dart';
