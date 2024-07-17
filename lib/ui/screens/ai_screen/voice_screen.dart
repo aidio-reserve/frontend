098af22b0d8af890c5eac0d6b11d95fd271fdfd5
yumayuma708 <yumaskywalker708@gmail.com>
2024-07-18 00:18:48 +0900

    その他リファクタリング

--- lib/ui/screens/ai_screen/voice_screen.dart
+++ lib/ui/screens/ai_screen/voice_screen.dart
@@ -43,7 +43,6 @@ class VoiceScreenState extends ConsumerState<VoiceScreen> {
     final messages = ref.watch(messageListProvider);
     final message = (messages.length % 2 == 0) ? null : messages.last;
 
-    // messageが更新された場合のみ読み上げる
     if (message != null &&
         message.text != lastSpokenMessage &&
         messages.length != 1) {
