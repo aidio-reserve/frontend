249920b6b78f0c6262f8603d7050292f0540471c
yumayuma708 <yumaskywalker708@gmail.com>
2024-07-30 20:28:34 +0900

    デプロイ

--- lib/models/HotelReviews/hotel_review.dart
+++ lib/models/HotelReviews/hotel_review.dart
@@ -1,4 +1,4 @@
-import 'package:aitrip/Converters/timestamp_converter.dart';
+import 'package:aitrip/converters/timestamp_converter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
